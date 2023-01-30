326c7f2bb22c7869728db4912876c9f77f8b78c8
jaykv <improcuziam@gmail.com>
2023-01-29 23:08:57 -0500

    Reformat

--- commandeer/clis/hello.py
+++ commandeer/clis/hello.py
@@ -1,16 +1,21 @@
 ## Generated hello on 2023-01-29 23:03:41.519982
 
-import rich_click as click; import subprocess; CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help']);
-        
+import rich_click as click
+import subprocess
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option('0.1.0')
 def cli():
     pass
 
+
 @cli.command()
 def bash():
     """Help for bash"""
-    subprocess.run(["echo","hello from bash"])
+    subprocess.run(["echo", "hello from bash"])
 
 
 @cli.command()
