0a112652e63917f0821748ae784447b6d5b6ebde
jaykv <improcuziam@gmail.com>
2023-05-14 23:35:26 -0400

    escape ansi in tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,19 @@
+import re
+
 from click.testing import CliRunner
 from cliffy.cli import cli, render, init, run_cli
 from cliffy.homer import get_metadata
+
+
+def escape_ansi(line: str) -> str:
+    return re.compile(r'(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]').sub('', line)
 
 
 def test_cli_help():
     runner = CliRunner()
     result = runner.invoke(cli, ['--help'])  # type: ignore
     assert result.exit_code == 0
-    assert 'Usage: cli' in result.output
+    assert 'Usage: cli' in escape_ansi(result.output)
 
 
 def test_cli_version():
@@ -20,14 +26,14 @@ def test_cli_init():
     runner = CliRunner()
     result = runner.invoke(init, ['hello', '--render'])
     assert result.exit_code == 0
-    assert 'name: hello' in result.output
+    assert 'name: hello' in escape_ansi(result.output)
 
 
 def test_cli_render():
     runner = CliRunner()
     result = runner.invoke(render, ['examples/town.yaml'])
     assert result.exit_code == 0
-    assert 'cli = typer.Typer' in result.output
+    assert 'cli = typer.Typer' in escape_ansi(result.output)
     assert get_metadata("town") is None
 
 
@@ -35,5 +41,5 @@ def test_cli_run():
     runner = CliRunner()
     result = runner.invoke(run_cli, ['examples/hello.yaml', '--', '-h'])
     assert result.exit_code == 0
-    assert 'Usage: hello_' in result.output
+    assert 'Usage: hello_' in escape_ansi(result.output)
     assert get_metadata('hello') is None
